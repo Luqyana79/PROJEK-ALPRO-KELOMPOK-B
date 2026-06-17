0e752cdf1a228a0c7958dc89b23054380198a92c
intanmalika19 <intanmalikasalsabila@gmail.com>
2026-06-17 14:11:59 +0700

    Create | |--02_String_kalimat.R

---
+++ |-- scr_R/| |--02_String_kalimat.R
@@ -0,0 +1,10 @@
+#bentuk umun
+K1 = "masukkan teks."
+K2 = 'Masukkan teks, "Masukkan teks."'
+K3 = "\"Masukkan teks\""
+K4 = "Masukkan teks."
+
+cat(K1, "\n")
+cat(K2, "\n")
+cat(K3, "\n")
+cat(K4, "\n")
